4b8b31ba2cd4896c333e293bb48bc1fe98ae48eb
zyileven <zyileven@gmail.com>
2024-09-03 21:29:23 +0800

    实现搜索和分页

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,20 +1,23 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
-import { useSearchParams } from 'next/navigation';
+import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+import { useDebouncedCallback } from "use-debounce";
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
-
-  const handleSearch = (term: string) => {
+  const pathname = usePathname()
+  const { replace } = useRouter()
+  const handleSearch = useDebouncedCallback((term: string) => { // 主要是根据搜索字符更改 url
     const params = new URLSearchParams(searchParams);
+    params.set('page', '1');
     if (term) {
       params.set('query', term);
     } else {
       params.delete('query');
     }
-    console.log(term);
-  }
+    replace(`${pathname}?${params.toString()}`)
+  }, 300)
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
@@ -27,6 +30,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
         onChange={(e) => {
           handleSearch(e.target.value);
         }}
+        defaultValue={searchParams.get('query')?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
